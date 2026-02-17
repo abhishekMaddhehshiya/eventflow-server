21b2f6d6c9d601a7cb038cda496305932e9a6bde
abhishekMaddhehshiya <abhishekmaddheshiya829@gmail.com>
2026-02-17 17:27:59 +0530

    route added

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,8 @@ import cors from 'cors'
 import authRoutes from './routes/auth';
 
 import connectDB from './lib/db';
+import userRoutes from './routes/user';
+import organizerRoutes from './routes/organizers';
 const port = 3000;
 
 const app = express();
@@ -17,6 +19,8 @@ app.use(
 connectDB();
 
 app.use("/api/v1/auth", authRoutes);
+app.use("/api/v1/user", userRoutes);
+app.use("/api/v1/organizer", organizerRoutes);
 
 app.listen(port, () => {
   console.log("app is running at port: ", port);
